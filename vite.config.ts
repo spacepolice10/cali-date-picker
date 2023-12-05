3963643c57eb8d62167cfca99ee42227ea12fde3
spacepolice10 <spcpolice@arbina.com>
2023-12-06 01:47:07 +0600

    init: base preparations (packages, tsconfig)

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react/jsx-runtime": "jsxRuntime",
+          "react-dom": "reactDom",
+        },
+      },
+    },
+  },
+});
